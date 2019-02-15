3331585e7eb532a61429fa63568a1648144276d7
Jeff <6pickles@gmail.com>
2019-02-15 16:34:43 -0600

    Added license info.

--- src/Common/Hardware_Abstraction_Layer/AVR_328/control_type_spindle.h
+++ src/Common/Hardware_Abstraction_Layer/AVR_328/control_type_spindle.h
@@ -1,9 +1,24 @@
 /*
-* control_type_spindle.h
+*  c_control_type_spindle.h - NGC_RS274 controller.
+*  A component of Talos
 *
-* Created: 2/12/2019 10:10:23 PM
-* Author: jeff_d
+*  Copyright (c) 2016-2019 Jeff Dill
+*
+*  Talos is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU LPLPv3 License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  Talos is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with Talos.  If not, see <http://www.gnu.org/licenses/>.
 */
+
+
 #ifdef __AVR_ATmega328P__
 
 #ifndef __CONTROL_TYPE_SPINDLE_H__
