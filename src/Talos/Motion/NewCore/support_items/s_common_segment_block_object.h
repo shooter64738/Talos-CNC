38c7dc59e9049b7d18a5b231d6e87c6504b72823
shooter64738 <6pickles@gmail.com>
2020-05-26 15:50:46 -0500

    Fixed control bits carry up

--- src/Talos/Motion/NewCore/support_items/s_common_segment_block_object.h
+++ src/Talos/Motion/NewCore/support_items/s_common_segment_block_object.h
@@ -5,10 +5,17 @@
 #include "s_common_all_object.h"
 #include "s_bresenham_item.h"
 
+struct s_segment_control_bits
+{
+	s_bit_flag_controller<e_system_block_state> system;
+	s_bit_flag_controller<e_feed_block_state> feed;
+	s_bit_flag_controller<e_speed_block_state> speed;
+};
 
 struct s_common_segment_block_object
 {
 	s_common_all_object tracking;
+	s_segment_control_bits control_bits;
 	s_bresenham* bres_obj;	//<-- This is handled as a pointer so that if the buffer
 							//head or tail are moved it wont matter. We no longer
 							//need to track head and tail values to ensure this item
